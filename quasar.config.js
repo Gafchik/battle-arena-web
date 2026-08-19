2d1d8a6c58f8ee28f1dbad76ec3ea79e0778ac97
Gafchik <dichajeka@gmail.com>
2026-08-19 21:50:18 +0300

    Switch to history-mode routing to avoid clashing with Telegram's initData URL hash

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
       // https://v2.quasar.dev/quasar-cli-vite/page-routing-with-vue-router#filename-based-routing
       // filenameBasedRouting: true,
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // 'hash' collides with Telegram's own use of the URL fragment for initData
       // vueRouterBase,
 
       // publicPath: '/',
